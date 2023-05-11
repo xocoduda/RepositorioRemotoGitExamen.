c8f1968554b12fc63e2a3f7a51246d54d0bbdf2d
xocoduda <juan_xoco@hotmail.com>
2023-05-11 18:03:27 +0200

    "Cambio subido"

--- ProyectoGitExamen/src/paqueteGitExamen/GitExamen.java
+++ ProyectoGitExamen/src/paqueteGitExamen/GitExamen.java
@@ -2,7 +2,7 @@ package paqueteGitExamen;
 
 public class GitExamen {
 	public static void main(String[]args) {
-		System.out.println("Estoy en un examen");
+		System.out.println("Subido a git");
 	}
 
 }
