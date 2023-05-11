1a9ddac81ad5bf39905fae9871c48cc3937d9c59
xocoduda <juan_xoco@hotmail.com>
2023-05-11 18:08:52 +0200

    Cambios rama1

--- ProyectoGitExamen/src/paqueteRama1/NuevaClase1.java
+++ ProyectoGitExamen/src/paqueteRama1/NuevaClase1.java
@@ -3,7 +3,7 @@ package paqueteRama1;
 public class NuevaClase1 {
 	public static void main(String[]args) {
 		System.out.println("Mensaje 1");
-		System.out.println("Mensaje 2");
+		System.out.println("Mensaje 2.");
 	}
 
 }
